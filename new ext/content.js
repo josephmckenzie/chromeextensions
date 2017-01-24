2803ccc4b20fd6c8a8d7f14b2c446025822e8852
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-01-24 16:59:36 -0500

    initial commit

---
+++ new ext/content.js
@@ -0,0 +1,18 @@
+//alert("Teecee and Coder are two pretty cool dogs, My doggie bubba was one cool dog too")
+
+
+//
+//   var MyDiv2 = document.getElementById('test').value;
+//  
+//alert(MyDiv2)
+
+
+
+
+var findtext = document.getElementById('test'),
+
+htmlContent = findtext.innerHTML,
+
+textContent = findtext.textContent;
+
+alert(textContent)
